9db305e6e7f61a756d41f2c6cfa9b5e79fdfcc7b
Thomas Aunvik <thaun1@live.no>
2018-11-09 20:54:19 +0100

    Update..

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+    [SerializeField] private TheButton button;
+
+    [SerializeField] private float maxRaycastDistance = 20;
+    [SerializeField] private KeyCode Mouse0Key = KeyCode.Mouse0;
+
+    void Start()
+    {
+        if(!cam) cam = Camera.main;
+        if(!button) button = FindObjectOfType<TheButton>();
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(Mouse0Key))
+        {
+            RaycastHit hit;
+            Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, maxRaycastDistance);
+            if (hit.transform)
+            {
+                if (hit.transform.GetComponent<TheButton>()) button.OnButtonDown();
+                else { button.OnButtonEnd(); }
+            }
+            else { button.OnButtonEnd(); }
+        }
+        if (Input.GetKey(Mouse0Key))
+        {
+            RaycastHit hit;
+            Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, maxRaycastDistance);
+            if (hit.transform)
+            {
+                if (hit.transform.GetComponent<TheButton>()) button.OnButtonHold();
+                else { button.OnButtonEnd(); }
+            }
+            else
+            {
+                button.OnButtonEnd();
+            }
+        }
+        else
+        {
+            button.OnButtonEnd();
+        }
+    }
+}
---
+++ Assets/Scripts/TheButton.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TheButton : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+    [SerializeField] private Animator animator;
+
+    private float whenButtonIsPressed;
+    private bool isPressing = false;
+
+    void Start()
+    {
+        if (!animator) animator = GetComponent<Animator>();
+    }
+    
+    void Update()
+    {
+        if (isPressing)
+        {
+            float time = Time.timeSinceLevelLoad - whenButtonIsPressed;
+            slider.value = time;
+        }
+        else
+        {
+            slider.value = Mathf.Lerp(slider.value, 0, 0.5f);
+        }
+    }
+
+    public void OnButtonHold()
+    {
+
+    }
+
+    public void OnButtonDown()
+    {
+        whenButtonIsPressed = Time.timeSinceLevelLoad;
+        isPressing = true;
+        animator.SetBool("isPressing", isPressing);
+    }
+
+    public void OnButtonEnd()
+    {
+        isPressing = false;
+        animator.SetBool("isPressing", isPressing);
+    }
+}
